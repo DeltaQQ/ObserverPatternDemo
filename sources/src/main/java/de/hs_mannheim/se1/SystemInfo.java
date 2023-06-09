cb1459ea9aabb3a452a05fb3b65ca9f375fe2b20
Michael Pochtar <michael.pochtar@stud.hs-mannheim.de>
2023-06-09 17:07:45 +0200

    Basic JavaFX Maven setup

---
+++ sources/src/main/java/de/hs_mannheim/se1/SystemInfo.java
@@ -0,0 +1,13 @@
+package de.hs_mannheim.se1;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ sources/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module de.hs_mannheim.se1 {
+    requires javafx.controls;
+    exports de.hs_mannheim.se1;
+}
